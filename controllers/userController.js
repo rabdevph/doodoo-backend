02d8927418ce40bcbd235dca45a707f96f62fc9a
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-12-18 09:25:48 +0400

    Add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/userModel');
+
+// @desc    Register new user
+// @route   POST /api/users
+// @access  Public
+const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.register(res, name, email, password);
+
+    if (user) {
+      res.status(200).json({
+        _id: user._id,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data.');
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { register };
