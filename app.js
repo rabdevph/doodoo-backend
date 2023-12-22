2c1843cbcd31416be8b049a8937f03f97307f991
Ralph Arsy Bauto <rab.devph@gmail.com>
2023-12-22 22:54:35 +0400

    Update cors middleware

--- app.js
+++ app.js
@@ -14,7 +14,20 @@ connectToDb();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow listed origins
+      const allowedOrigins = ['http://localhost:3000'];
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed connection'));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
