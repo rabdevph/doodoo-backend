bd4446df62a2a469ad612c57e977608ccfddc1c3
Ralph Arsy Bauto <rab.devph@gmail.com>
2024-01-15 15:29:45 +0400

    Add validation

--- models/userModel.js
+++ models/userModel.js
@@ -22,12 +22,68 @@ const userSchema = mongoose.Schema(
 );
 
 userSchema.statics.register = async function (res, name, email, password) {
-  const userExists = await this.findOne({ email });
+  const errorFields = [];
 
-  // User already exists
-  if (userExists) {
+  if (!name) {
+    errorFields.push('name');
+  }
+
+  if (!email) {
+    errorFields.push('email');
+  }
+
+  if (!password) {
+    errorFields.push('password');
+  }
+
+  // Check empty fields
+  if (errorFields.length > 0) {
     res.status(400);
-    throw new Error('User already exists.');
+    const errorMessage =
+      errorFields.length > 1
+        ? `Please enter your ${errorFields.slice(0, -1).join(', ')} and ${errorFields.slice(-1)}.`
+        : `Please enter your ${errorFields[0]}.`;
+    const error = new Error(errorMessage);
+    error.errorFields = errorFields;
+    throw error;
+  }
+
+  // Check if email is valid
+  if (!validator.isEmail(email)) {
+    errorFields.push('email');
+    res.status(400);
+    const error = new Error('Invalid email address.');
+    error.errorFields = errorFields;
+    throw error;
+  }
+
+  // Check if password is strong
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+  ) {
+    errorFields.push('password');
+    res.status(400);
+    const error = new Error(
+      'Password must be at least 8 characters long and contain at least 1 - lowercase and uppercase letter, number and special character.',
+    );
+    error.errorFields = errorFields;
+    throw error;
+  }
+
+  // Check if user already exists
+  const userExists = await this.findOne({ email });
+  if (userExists) {
+    errorFields.push('email');
+    res.status(400);
+    const error = new Error('User already exists.');
+    error.errorFields = errorFields;
+    throw error;
   }
 
   const salt = await bcrypt.genSalt(10);
@@ -39,18 +95,46 @@ userSchema.statics.register = async function (res, name, email, password) {
 };
 
 userSchema.statics.login = async function (res, email, password) {
+  const errorFields = [];
+
+  if (!email) {
+    errorFields.push('email');
+  }
+
+  if (!password) {
+    errorFields.push('password');
+  }
+
+  // Check empty fields
+  if (errorFields.length > 0) {
+    res.status(400);
+    const errorMessage =
+      errorFields.length > 1
+        ? `Please enter your ${errorFields.slice(0, -1).join(', ')} and ${errorFields.slice(-1)}.`
+        : `Please enter your ${errorFields[0]}.`;
+    const error = new Error(errorMessage);
+    error.errorFields = errorFields;
+    throw error;
+  }
+
   const user = await this.findOne({ email });
 
   if (!user) {
+    errorFields.push('email');
     res.status(401);
-    throw new Error('Incorrect email address.');
+    const error = new Error('Incorrect email address.');
+    error.errorFields = errorFields;
+    throw error;
   }
 
   const match = await bcrypt.compare(password, user.password);
 
   if (!match) {
+    errorFields.push('password');
     res.status(401);
-    throw new Error('Incorrect password.');
+    const error = new Error('Incorrect password.');
+    error.errorFields = errorFields;
+    throw error;
   }
 
   return user;
