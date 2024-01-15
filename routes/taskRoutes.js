b1ee74b488956e0338a723dc48d5e4a9adfb2877
Ralph Arsy Bauto <rab.devph@gmail.com>
2024-01-15 15:30:35 +0400

    Add new routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getAll, create } = require('../controllers/taskController');
+const { getAll, createTask, deleteTask, updateTask } = require('../controllers/taskController');
 const auth = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', auth, getAll);
-router.post('/', auth, create);
+router.post('/', auth, createTask);
+router.delete('/:id', auth, deleteTask);
+router.patch('/:id', auth, updateTask);
 
 module.exports = router;
