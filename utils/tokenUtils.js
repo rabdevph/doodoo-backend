370da7b169dde515728244128c90658bb6d04410
Ralph Arsy Bauto <rab.devph@gmail.com>
2024-01-15 15:22:32 +0400

    Modify parameter

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-const generateTokens = (_id) => {
-  const at = jwt.sign({ _id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 5 * 60 });
-  const st = jwt.sign({ _id }, process.env.SESSION_TOKEN_SECRET, { expiresIn: 12 * 60 * 60 });
+const generateTokens = (userDetails) => {
+  const at = jwt.sign({ userDetails }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 5 * 60 });
+  const st = jwt.sign({ userDetails }, process.env.SESSION_TOKEN_SECRET, {
+    expiresIn: 12 * 60 * 60,
+  });
 
   return { at, st };
 };
